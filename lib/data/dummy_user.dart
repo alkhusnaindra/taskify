aae48c98cbab998cf796d2e63b1c6a8ac2da3b2a
IndraMaulana <alkhusnaindra1@gmail.com>
2025-12-19 09:30:46 +0700

    setup project

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,5 @@
+const dummyUser = { 
+  "email" : "user@example.com",
+  "password" : "password123",
+  "name" : "John Doe"
+};
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+import 'pages/home_page.dart';
+import 'pages/profile_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tasky',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/profile': (context) => const ProfilePage(),
+      },
+    );
+  }
+}
